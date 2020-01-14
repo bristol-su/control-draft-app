3a63de559f42c2df10d2ee50fdba58f8c58aa7f3
Toby Twigger <tobytwigger1@gmail.com>
2020-01-14 10:49:11 +0000

    Remove factories causing 500 error

--- database/factories/GroupFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$factory->define(\App\Models\Group::class, function(\Faker\Generator $faker) {
-    return [
-        'name' => $faker->unique()->company,
-        'email' => $faker->unique()->email
-    ];
-});
--- database/factories/UserFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$factory->define(\App\Models\User::class, function(\Faker\Generator $faker) {
-    return [
-        'forename' => $faker->firstName,
-        'surname' => $faker->lastName,
-        'email' => $faker->unique()->email
-    ];
-});
